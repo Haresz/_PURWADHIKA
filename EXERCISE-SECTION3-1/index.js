1116329d40674e8139e5ec9386b7f8f9b0283dc2
haris <89023322+Haresz@users.noreply.github.com>
2024-01-24 21:59:24 +0700

    exercise-section3-1

---
+++ EXERCISE-SECTION3-1/index.js
@@ -0,0 +1,7 @@
+const detachedNum = prompt("Masukan Angka")
+
+if (detachedNum % 2 == 0) {
+    alert(`Angka ini ${detachedNum} adalah bilangan genap`)
+} else {
+    alert(`Angka ini ${detachedNum} adalah bilangan ganjil`)
+}
