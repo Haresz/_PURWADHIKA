b465f68da211ef5287d6ccc0ddcf98ea4ae21179
haris <89023322+Haresz@users.noreply.github.com>
2024-01-25 22:02:18 +0700

    exercise 1 section 4 loop 2 done

--- EXERCISE-SECTION4-1/index.js
+++ EXERCISE-SECTION4-1/index.js
@@ -15,21 +15,37 @@ let star2 = ""
 let number2 = 10
 
 for (let i = 1; i <= number2; i++) {
-    for (let j = 0; j <= Math.ceil(number2 / 2) - i; j++) {
+    number2 % 2 == 0 ? number2 += 1 : number2 = number2
+    // MATH CEIL 5
+    for (let j = 0; j <= Math.round(number2 / 2) - i; j++) {
         star2 += " *"
-        if (i == Math.ceil(number2 / 2)) {
+        if (i == Math.round(number2 / 2)) {
             break
         }
     }
-    if (i >= Math.ceil(number2 / 2)) {
-        for (let k = 1; k <= i - Math.ceil(number2 / 2); k++) {
-            star2 += " *"
+    if (i >= Math.round(number2 / 2) + 1) {
+        for (let k = 1; k <= i - Math.round(number2 / 2) + 1; k++) {
+            star2 += " *."
         }
     }
     star2 += "\n"
+
+}
+
+
+let star = ""
+let number = 5
+
+for (let i = 1; i <= number; i++) {
+    for (let j = 1; j <= i + 1; j++) {
+        star += " *"
+    } star += "\n"
 }
 
 console.log(star2)
+
+
+//Benr
 
 
 
