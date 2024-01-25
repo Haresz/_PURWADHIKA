7b0f6018bc96baaaef6d20b426e16a359c603328
haris <89023322+Haresz@users.noreply.github.com>
2024-01-25 23:41:21 +0700

    exercise section 4 done

--- EXERCISE-SECTION4-1/index.js
+++ EXERCISE-SECTION4-1/index.js
@@ -11,42 +11,96 @@
 // console.log(star)
 
 // Bentuk Dua
-let star2 = ""
-let number2 = 10
+// let star2 = ""
+// let number2 = 10
 
-for (let i = 1; i <= number2; i++) {
-    number2 % 2 == 0 ? number2 += 1 : number2 = number2
-    // MATH CEIL 5
-    for (let j = 0; j <= Math.round(number2 / 2) - i; j++) {
-        star2 += " *"
-        if (i == Math.round(number2 / 2)) {
-            break
-        }
+// for (let i = 1; i <= number2; i++) {
+//     number2 % 2 == 0 ? number2 += 1 : number2 = number2
+//     // MATH CEIL 5
+//     for (let j = 0; j <= Math.round(number2 / 2) - i; j++) {
+//         star2 += " *"
+//         if (i == Math.round(number2 / 2)) {
+//             break
+//         }
+//     }
+// if (i >= Math.round(number2 / 2) + 1) {
+//     for (let k = 1; k <= i - Math.round(number2 / 2) + 1; k++) {
+//         star2 += " *."
+//     }
+// }
+//     star2 += "\n"
+
+// }
+
+//Bentuk 3
+
+let star3 = ""
+let number3 = 9
+
+for (let i = 1; i <= number3; i++) {
+
+    for (let j = 0; j <= number3 - i; j++) {
+        star3 += "   "
     }
-    if (i >= Math.round(number2 / 2) + 1) {
-        for (let k = 1; k <= i - Math.round(number2 / 2) + 1; k++) {
-            star2 += " *."
-        }
+
+    for (let k = 1; k <= i * 2 - 1; k++) {
+        star3 += ""
+        star3 += " * "
+        star3 += ""
+    } star3 += "\n"
+}
+
+console.log(star3)
+
+// Bentuk 4
+
+let star4 = ""
+let number4 = 9
+
+for (let i = 0; i <= number4; i++) {
+
+    for (let j = 0; j <= i; j++) {
+        star4 += "   "
     }
-    star2 += "\n"
+
+    for (let k = 0; k <= (number4 * 2 - 1) - (i * 2 + 1); k++) {
+        star4 += " * "
+    } star4 += "\n"
+}
+
+console.log(star4)
+
+// Bentuk 5
+
+let star5 = ""
+let number5 = 10
+
+for (let i = 1; i <= number5; i++) {
+    number5 % 2 == 1 ? number5 += 1 : number5 = number5
+    if (i <= Math.round(number5 / 2)) {
+        for (let j = 0; j <= number5 - i; j++) {
+            star5 += "   "
+        }
+
+        for (let k = 1; k <= i * 2 - 1; k++) {
+            star5 += ""
+            star5 += " * "
+            star5 += ""
+        } star5 += "\n"
+    }
+    if (i > Math.round(number5 / 2)) {
+        for (let j = 0; j <= i; j++) {
+            star5 += "   "
+        }
+
+        for (let k = 0; k <= (number5 * 2 - 1) - (i * 2 + 1); k++) {
+            star5 += " *."
+        } star5 += "\n"
+    }
 
 }
 
-
-let star = ""
-let number = 5
-
-for (let i = 1; i <= number; i++) {
-    for (let j = 1; j <= i + 1; j++) {
-        star += " *"
-    } star += "\n"
-}
-
-console.log(star2)
-
-
-//Benr
-
+console.log(star5)
 
 
 
