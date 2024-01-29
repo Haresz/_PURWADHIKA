9daf3745d843e9378b9bca513dbdde4d5e52e095
haris <89023322+Haresz@users.noreply.github.com>
2024-01-29 11:19:36 +0700

    exercise section 6.6 done

---
+++ EXERCISE-SECTION6-6/index.js
@@ -0,0 +1,14 @@
+const reverseWord = (words) => {
+    let arr = words.split(' ')
+    const arrMap = arr.map((a) => {
+        let result = a.split('')
+        result = result.reverse()
+        result = result.join('')
+        return result
+    })
+    return arrMap.join(' ')
+}
+
+console.log(
+    reverseWord("Hai aku join mir")
+)
