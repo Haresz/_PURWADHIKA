c54fb2d0cefacb7a32667bffa94f3789510ace73
Haresz <rizalfikri852@gmail.com>
2024-02-01 10:06:55 +0700

    esercise-31-1-2024 Done

--- _workhome/exercise-31-1-2024/index.js
+++ _workhome/exercise-31-1-2024/index.js
@@ -42,4 +42,78 @@ const removeStringFromSearch = (word, search) => {
 
 // removeStringFromSearch("Hello world", "ell");
 
-const camelCaseConvert = (word) => {};
+const camelCaseConvert = (word) => {
+  let result = "";
+  let subWords = "";
+  word = word.split(" ");
+  for (let index = 0; index < word.length; index++) {
+    word[index][0].toLowerCase()
+      ? (subWords += word[index][0].toLocaleUpperCase())
+      : null;
+    for (let j = 1; j < word[index].length; j++) {
+      subWords += word[index][j];
+    }
+    result = result.concat(" ", subWords);
+    subWords = "";
+  }
+  return console.log(result);
+};
+
+// camelCaseConvert("hello word press");
+
+const reverseString = (word) => {
+  let result = "";
+  for (let index = word.length - 1; index >= 0; index--) {
+    result += word[index];
+  }
+  return console.log(result);
+};
+
+// reverseString("Hello");
+
+const stringConvert = (word) => {
+  let result = "";
+  for (let index = 0; index < word.length; index++) {
+    word[index] == word[index].toLowerCase()
+      ? (result += word[index].toLocaleUpperCase())
+      : (result += word[index].toLowerCase());
+  }
+  return console.log(result);
+};
+
+// camelCaseConvert("Hello Word Press");
+
+const findTheLargers = (num1, num2) => {
+  return console.log(Math.max(num1, num2));
+};
+
+// findTheLargers(190, 99);
+
+const numSmalToBig = (num1, num2, num3) => {
+  const numbers = [num1, num2, num3];
+  return console.log(
+    numbers.sort((a, b) => {
+      return a - b;
+    })
+  );
+};
+
+// numSmalToBig(7, 4, 19);
+
+const findTypeData = (data) => {
+  let result;
+  typeof data == "string"
+    ? (result = 1)
+    : typeof data == "number"
+    ? (result = 2)
+    : (result = 3);
+  return console.log(result);
+};
+
+// findTypeData("word");
+
+const replaceA = (word) => {
+  return console.log(word.toLowerCase().replace(/a/g, "*"));
+};
+
+replaceA("Halo my name is haris");
